f20632fb46124d4908cdd50ef979b8be16a7d066
victormc13 <viktorsb14@gmail.com>
2023-07-09 17:15:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java-Basic
+This repository is created specifically for practicing and learning the basics of the Java programming language within the OpenBootcamp platform.
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        String[] welcome = {"Hello", "world", "and", "welcome", "to", "Java-Basic!"};
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (String s : welcome) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.print(s + " ");
+        }
+    }
+}
