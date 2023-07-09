aa5e891eb5f97c2df29fd9fdd93d3c2add58f055
victormc13 <viktorsb14@gmail.com>
2023-07-09 18:14:12 -0500

    Add comments and documentation

---
+++ src/Concepts/Comments.java
@@ -0,0 +1,31 @@
+package Concepts;
+
+/**Documentation
+ * Main function entry point to Class Comments
+ * @author Victor Miñope Cielo
+ * @see Comments (another class that we have already created)
+ */
+
+public class Comments {
+    /**
+     * Main function entry point to application
+     * @param args args to receive
+     * @since 1.0
+     */
+    public static void main(String[] args) {
+
+        //Line comment
+
+        /*
+        Block comment
+
+        (Useful for document the steps of your code)
+        1. Open file
+        2. Read file
+        3. Assert a file
+        4. Close file
+         */
+
+        System.out.println("Comments in Java"); // print java comments
+    }
+}
