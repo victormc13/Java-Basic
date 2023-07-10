33b173be610e395cc1a6e1de3c937b77aceae9e7
victormc13 <viktorsb14@gmail.com>
2023-07-09 19:00:03 -0500

    Add variables and data types

---
+++ src/Concepts/Variables.java
@@ -0,0 +1,37 @@
+package Concepts;
+
+public class Variables {
+    public static void main(String[] args) {
+        /* int number = 13; */      //type identifier = value;
+        /* int number2;    */      //type identifier;
+        /* number2 = 14;  */      //identifier = value;
+
+        // integers
+        byte number1 = 1;     //1 byte
+        short number2 = 2;   //2 bytes
+        int number3 = 3;    //4 bytes
+        long number4 = 4L; //8 bytes
+
+        // float
+        float decimal1 = 4.9f;     //4 bytes
+        double decimal2 = 9.99d;  //8 bytes
+
+        // characters
+        char character1 = 'V';
+
+        // boolean
+        boolean truth = true;
+        boolean falsy = false;
+
+        // string of text
+        String name = "Victor";
+        String lastName = "Miñope";
+
+        // wrapper type
+        Integer wrapperNumber = null;          // No value has been assigned yet
+        Long wrapperLong = null;              // No value has been assigned yet
+        Float wrapperFloat = null;           // No value has been assigned yet
+        Boolean wrapperBoolean = null;      // No value has been assigned yet
+        Character wrapperCharacter = null; // No value has been assigned yet
+    }
+}
